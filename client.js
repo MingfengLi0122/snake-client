4441a90fc00d47326fd9ecaaf8c175ecfd213c05
Mingfeng Li <lmf950122@gmail.com>
2021-06-02 03:03:44 +0000

    Refactor with ES6

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = () => {
   const conn = net.createConnection({
     host: "10.0.2.15",
     port: 50541
   });
-  
+
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Hi server!")
@@ -15,4 +15,4 @@ const connect = function () {
 };
 
 
-module.exports = connect;
+module.exports = {connect};
